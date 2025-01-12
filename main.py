91eb49e480cccf93f975624f4cb9f50ccf3f5a3f
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 <ldxi1991@gmail.com>
2025-01-12 03:41:59 +0300

    adding authorize middleware

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ app.include_router(ShopAPIRouter)
 app.include_router(SellerAPIRouter)
 app.include_router(ProductAPIController)
 
+#creating authorization middleware
+@app.middleware("http")
+async def authorization_middleware(request:Request, call_next):
+    json_data = request.json()
+    if request.url.path == "/" or request.url.path == "/user":
+        response = await call_next(request)
+        return response
+    return "403"
 
 # index page endpoint
 @app.get("/")
